833b220d7ab3a88a7493ce24c8648176000f606d
Nicolas <nicolasm15@hotmail.com>
2020-06-09 17:58:44 -0300

    feat: add database files and migrations

---
+++ src/database/index.js
@@ -0,0 +1,40 @@
+import Sequelize from 'sequelize'; // Realizar a conexão com o banco de dados
+import mongoose from 'mongoose';
+
+import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
+
+import databaseConfig from '../config/database';
+
+const models = [User, File, Appointment];
+
+class Database {
+  constructor() {
+    this.init();
+    this.mongo();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models)
+      );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://172.17.0.2:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
+  }
+}
+
+export default new Database();
